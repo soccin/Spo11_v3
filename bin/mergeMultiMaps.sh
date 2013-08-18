ee767bf96d37c16d01a53e7304cfdcd59ee6d93c
Nicholas D. Socci <socci@lux>
2013-08-17 21:09:14 -0400

    Pass output file name from command line

--- bin/mergeMultiMaps.sh
+++ bin/mergeMultiMaps.sh
@@ -2,7 +2,7 @@
 DIR=$1
 base=$(echo $DIR | perl -ne 'm|/Sample_([^/]*)|; print "s_$1";')
 file1=$(ls $DIR/*MULTI.map|head -1)
-OUT=${base}___MULTI_FILT.map
+OUT=$2
 echo "OUT="$OUT
 head -1 $file1 >$OUT
 ls $DIR/*MULTI.map | xargs fgrep -hv mixer | sort -S 80g -T /scratch/socci -k1,1 -k2,2n >>$OUT
