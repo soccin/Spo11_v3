5c110888e0eb722403732d4d6d820cb236068199
Nicholas D. Socci <socci@lux>
2013-08-17 21:25:55 -0400

    Made processDirs more general

--- processDirs.sh
+++ processDirs.sh
@@ -1,7 +1,8 @@
 for dir in `cat DIRS`; do
 	sample=$(echo $dir | pyp 's[-1]' | sed 's/Sample_/s_/'); echo $sample;
    	mkdir -p $sample;
+	cp spo11.sh $sample
    	cd $sample;
-   	bsub -N SPO11.MM10 ../Spo11.MM10/pipe.sh ../$dir;
+   	bsub -N SPO11 ../../Spo11_v3/pipe.sh $dir;
    	cd ..;
 done
