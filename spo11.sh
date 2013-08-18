1663ce8afbf81924e2d6d780dbdeef07ad29fec8
Nicholas D. Socci <socci@lux>
2013-08-17 21:26:17 -0400

    Removed PARAMETERS.INI and change to spo11.sh for example

---
+++ spo11.sh
@@ -0,0 +1,15 @@
+##For adapter clipping Mouse=20, Yeast=15
+
+MIN_CLIP_LEN=15
+
+##GENOME & GENOME TAG 
+GENOME=/ifs/data/bio/Genomes/S.cerevisiae/sacCer2/SGD/20080628/SGD_sacCer2.fasta
+GTAG=SGD_sacCer2
+
+#GENOME=/ifs/data/bio/Genomes/M.musculus/mm10/mouse_mm10__All.fa
+#GTAG=mouse_mm10__All
+#GENOME=/home/socci/Work/Keeney/LamI/S.mikatae/CLEAN/sacMik.fa
+#GTAG=sacMik
+#GENOME=/home/socci/Work/Keeney/LamI/S.kudriavzevii/CLEAN/sacKud.fa
+#GTAG=sacKud
+
