70dede744c11d1c7f7c857b83461387db72d5f0e
Nicholas D. Socci <socci@lux>
2013-08-17 21:09:14 -0400

    Pass output file name from command line

--- bin/mergeMaps.sh
+++ bin/mergeMaps.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+
 DIR=$1
 base=$(echo $DIR | perl -ne 'm|/Sample_([^/]*)|; print "s_$1";')
 file1=$(ls $DIR/*UNIQUE.map|head -1)
-OUT=${base}___UNIQUE_FILT.map
-
+OUT=$2
 echo "OUT="$OUT
-
 head -1 $file1 >$OUT
 ls $DIR/*UNIQUE.map | xargs fgrep -hv mixer | sort -T /scratch/socci -k1,1 -k2,2n >>$OUT
 
