534ec3d0f89ddea0dbb798306e1cae8509e6dd18
Nicholas D. Socci <socci@lux>
2013-08-17 21:08:12 -0400

    Pass in MIN_READ_LEN from command line

--- bin/spo11_Pipeline01.sh
+++ bin/spo11_Pipeline01.sh
@@ -9,7 +9,6 @@ BIN=$SDIR
 # Clipping Parameters
 #
 ADAPTER=AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC
-MIN_CLIP_LEN=20
 
 SHRIMP_FOLDER=/home/socci/Work/SeqAna/Mappers/SHRiMP/2_1_1/SHRiMP_2_1_1b
 GMAPPER=/ifs/data/socci/Work/SeqAna/Mappers/SHRiMP/2_1_1/SHRiMP_2_1_1b/bin/gmapper-ls
@@ -23,18 +22,18 @@ FASTQ=$1
 GENOMETAG=$2
 GENOME=$3
 GENOME_INDEX=$(dirname $GENOME)/SHRiMP/DNA/$GENOMETAG-ls
-
 OUTFOLDER=$4
+MIN_CLIP_LEN=$5
 
 FOLDER=${FASTQ%/*}
 BASE=${FASTQ##*/}
 BASE=${BASE%%.*}
 BASE=$OUTFOLDER/$BASE
 
-zcat $FASTQ | /ifs/data/socci/opt/bin/fastx_clipper -a $ADAPTER -l $MIN_CLIP_LEN -n -v -Q33 -i - \
+zcat $FASTQ | head -40000 | /ifs/data/socci/opt/bin/fastx_clipper -a $ADAPTER -l $MIN_CLIP_LEN -n -v -Q33 -i - \
     | $BIN/splitMixer.py > ${BASE}___CLIPPED.fastq
 
-$GMAPPER -N 24 -U -g -1000 -q -1000 \
+$GMAPPER -N 12 -U -g -1000 -q -1000 \
     -m 10 -i -20 -h 100 -r 50% \
     -n 1 \
     -L $GENOME_INDEX \
