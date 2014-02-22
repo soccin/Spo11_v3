b530f7a58c5d669a1597090ee7fc650368fa76e1
Nicholas D. Socci <socci@lux>
2014-02-22 15:34:33 -0500

    Added sKud_ZP591 and set DEFAULT genome to off

--- spo11.sh
+++ spo11.sh
@@ -2,9 +2,13 @@
 
 MIN_CLIP_LEN=15
 
+echo "PLEASE SET GENOME"
+echo 
+exit
+
 ##GENOME & GENOME TAG 
-GENOME=/ifs/data/bio/Genomes/S.cerevisiae/sacCer2/SGD/20080628/SGD_sacCer2.fasta
-GTAG=SGD_sacCer2
+#GENOME=/ifs/data/bio/Genomes/S.cerevisiae/sacCer2/SGD/20080628/SGD_sacCer2.fasta
+#GTAG=SGD_sacCer2
 
 #GENOME=/ifs/data/bio/Genomes/M.musculus/mm10/mouse_mm10__All.fa
 #GTAG=mouse_mm10__All
@@ -12,4 +16,6 @@ GTAG=SGD_sacCer2
 #GTAG=sacMik
 #GENOME=/home/socci/Work/Keeney/LamI/S.kudriavzevii/CLEAN/sacKud.fa
 #GTAG=sacKud
+#GENOME=/home/socci/Work/Keeney/LamI/S.kudriavzevii__ZP591/CLEAN/sacKud.ZP591.fa
+#GTAG=sacKud_ZP591
 
