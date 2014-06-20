eb782d8cb4827a74a917582f8805ab63c3bacbb3
Nicholas D. Socci <socci@lux>
2014-06-20 15:47:07 -0400

    Removed processDirs.sh

--- processDirs.sh
+++
@@ -1,8 +0,0 @@
-for dir in `cat DIRS`; do
-	sample=$(echo $dir | pyp 's[-1]' | sed 's/Sample_/s_/'); echo $sample;
-   	mkdir -p $sample;
-	cp spo11.sh $sample
-   	cd $sample;
-   	qsub -N SPO11 ~/Work/SGE/qCMD ../../Spo11_v3/pipe.sh $dir;
-   	cd ..;
-done
