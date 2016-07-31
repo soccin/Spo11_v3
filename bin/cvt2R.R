473915f8933556acecff3c667775def034795475
Nicholas D. Socci <soccin@mskcc.org>
2016-07-30 23:15:29 -0400

    But try block for empty files

--- bin/cvt2R.R
+++ bin/cvt2R.R
@@ -2,7 +2,9 @@ args <- commandArgs(trailingOnly = TRUE)
 outfile=cc(gsub("\\.map.*$","",args[1]),".Rdata")
 chrom=strsplit(args[1],"[,\\.]")[[1]][2]
 
+try({
 dd=read.delim((args[1]))
-
 save(dd,file=outfile,compress=T)
+})
+
 
