a50dc462476b287156efd6445e39cd95c419e32a
Nicholas D. Socci <soccin@mskcc.org>
2016-08-01 11:50:37 -0400

    Removed debugging statement

--- bin/spo11_Pipeline01.sh
+++ bin/spo11_Pipeline01.sh
@@ -41,7 +41,8 @@ BASE=$OUTFOLDER/$BASE
 #zcat $FASTQ | /ifs/data/socci/opt/bin/fastx_clipper -a $ADAPTER -l $MIN_CLIP_LEN -n -v -Q33 -i - \
 #    | $BIN/splitMixer.py > ${BASE}___CLIPPED.fastq
 
-zcat $FASTQ | head -1000000 | $BIN/fastqLenFilter.py 15 > ${BASE}___CLIPPED.fastq
+#zcat $FASTQ | head -100000 | $BIN/fastqLenFilter.py 15 > ${BASE}___CLIPPED.fastq
+zcat $FASTQ | $BIN/fastqLenFilter.py 15 > ${BASE}___CLIPPED.fastq
 
 $GMAPPER -N 18 -U -g -1000 -q -1000 \
     -m 10 -i -20 -h 100 -r 50% \
