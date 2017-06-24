a0d1f210caf8012f1ab0a8375507edc499beac15
Nicholas D. Socci <soccin@mskcc.org>
2017-06-24 19:57:24 -0400

    Added script to process mapping file Process all samples in mapping file. Handle case were more than one run per sample

---
+++ processMappingFile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SDIR="$( cd "$( dirname "$0" )" && pwd )"
+MAPPING=$1
+
+if [ "$#" -ne 1 ]; then
+    echo "usage processMappingFile.sh MAPPING_FILE"
+    exit
+fi
+
+for sample in $(cat $MAPPING | cut -f2 | sort | uniq); do
+    echo $sample;
+    cat $MAPPING | awk -v S=$sample '$2==S{print $4}' \
+        | xargs $SDIR/pipe.sh
+done
