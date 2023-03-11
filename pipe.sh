b290e59feb1d8ddec31046871042c50bac067969
Nicholas D. Socci <soccin@mskcc.org>
2023-03-11 13:17:52 -0500

    Added set -e to scripts

--- pipe.sh
+++ pipe.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 SDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ -e $SDIR/venv/bin/activate ]; then
--- processProjects.sh
+++ processProjects.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 SDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
